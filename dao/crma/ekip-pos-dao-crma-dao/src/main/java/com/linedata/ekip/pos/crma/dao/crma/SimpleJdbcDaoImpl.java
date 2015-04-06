d2947e7ac77eb60b993bd6c8667b42f2fd87b594
roidecyecle <saddemabdelfattah@gmail.com>
2015-04-06 18:55:50 +0100

    CH

---
+++ dao/crma/ekip-pos-dao-crma-dao/src/main/java/com/linedata/ekip/pos/crma/dao/crma/SimpleJdbcDaoImpl.java
@@ -0,0 +1,8 @@
+package com.linedata.ekip.pos.crma.dao.crma;
+
+import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+
+@SuppressWarnings("deprecation")
+public class SimpleJdbcDaoImpl extends SimpleJdbcDaoSupport {
+
+}
